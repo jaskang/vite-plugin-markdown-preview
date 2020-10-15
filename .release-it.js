d060b0a4ecf9108bff0a09f71f1ca3ec17bb2a5b
jaskang <kangchaomin@dayuwuxian.com>
2020-10-15 11:05:49 +0800

    chore: remove releaseNotes

--- .release-it.js
+++ .release-it.js
@@ -5,8 +5,7 @@ module.exports = {
       'npx auto-changelog --stdout --commit-limit false --unreleased --template https://raw.githubusercontent.com/release-it/release-it/master/templates/changelog-compact.hbs'
   },
   github: {
-    release: true,
-    releaseNotes: 'generate-release-notes.sh ${latestVersion} ${version}'
+    release: true
   },
   hooks: {
     'after:bump': 'npx auto-changelog -p'
