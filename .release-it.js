422a37c50bb63450d52aa954532f2bdb3f40e1f9
jaskang <jaskang@qq.com>
2022-05-17 11:30:03 +0800

    chore: release

--- .release-it.js
+++ .release-it.js
@@ -9,9 +9,6 @@ module.exports = {
     'before:init': 'pnpm build',
     'after:release': 'echo Successfully released ${name} v${version} to ${repo.repository}.',
   },
-  github: {
-    // release: true,
-  },
   plugins: {
     '@release-it/conventional-changelog': {
       preset: 'angular',
